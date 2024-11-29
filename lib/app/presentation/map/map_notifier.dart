1a8d6b2492e9a2e057550b4c90d8a66527e58264
Fatahul Ahmad Dzikri <fatahuldzikri@gmail.com>
2024-11-29 10:09:01 +0700

    Menambah validasi waktu ketika absensi

--- lib/app/presentation/map/map_notifier.dart
+++ lib/app/presentation/map/map_notifier.dart
@@ -8,6 +8,7 @@ import 'package:presensi_battuta_app/app/module/entity/schedule.dart';
 import 'package:presensi_battuta_app/app/module/use_case/attendance_send.dart';
 import 'package:presensi_battuta_app/app/module/use_case/schedule_banned.dart';
 import 'package:presensi_battuta_app/app/module/use_case/schedule_get.dart';
+import 'package:presensi_battuta_app/core/helper/date_time_helper.dart';
 import 'package:presensi_battuta_app/core/helper/location_helper.dart';
 import 'package:presensi_battuta_app/core/provider/app_provider.dart';
 
@@ -45,6 +46,7 @@ class MapNotifier extends AppProvider {
   void init() async {
     await _getEnableAndPermission();
     await _getSchedule();
+    if (errorMessage.isEmpty) await _checkSchedule();
   }
 
   _getEnableAndPermission() async {
@@ -79,6 +81,23 @@ class MapNotifier extends AppProvider {
       errorMessage = response.message;
     }
     hideLoading();
+  }
+
+  _checkSchedule() {
+    final now = DateTime.now();
+    final startTimeShift = _schedule!.shift.startTime.split(":");
+    final dateTimeShift = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        int.parse(startTimeShift[0]),
+        int.parse(startTimeShift[1]),
+        int.parse(startTimeShift[2]));
+    if (DateTimeHelper.getDifferenece(a: now, b: dateTimeShift) >
+        Duration(minutes: 30)) {
+      errorMessage =
+          'Kehadiran dapat dibuat paling cepat 30 menit sebelum jam kerja';
+    }
   }
 
   checkLocationPermission() async {
